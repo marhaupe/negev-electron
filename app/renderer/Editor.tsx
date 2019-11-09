155988868003ee219717e0ac64f6c4974dd12e33
marhaupe <marhaupe97@gmail.com>
2019-11-09 17:13:46 +0100

    prettier

--- app/renderer/Editor.tsx
+++ app/renderer/Editor.tsx
@@ -1,23 +1,20 @@
-import * as React from "react";
-import GraphiQL from "graphiql";
-import fetch from "isomorphic-fetch";
-import "graphiql/graphiql.css";
+import React, { useState } from 'react';
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
+import 'graphiql/graphiql.css';
 
-function usePersistedState<S>(
-  key: string,
-  initialState: S | (() => S)
-): [S, (value: S) => void] {
-  const [state, setState] = React.useState(() => {
+function usePersistedState<S>(key: string, initialState: S | (() => S)): [S, (value: S) => void] {
+  const [state, setState] = useState(() => {
     return getPersistedState(initialState);
   });
 
   function getPersistedState(initialState: S | (() => S)) {
     const persistedState = window.localStorage.getItem(key);
     if (!persistedState) {
-      if (typeof initialState !== "function") {
+      if (typeof initialState !== 'function') {
         return initialState;
       }
-      return (initialState as (() => S))();
+      return (initialState as () => S)();
     }
     return JSON.parse(persistedState);
   }
@@ -39,23 +36,23 @@ function calculateAverage(args: number[]) {
 }
 
 export function Editor() {
-  const [endpoint, setEndpoint] = usePersistedState("endpoint", "");
-  const [counts, setCount] = usePersistedState("counts", 100);
-  const [cycles, setCycles] = usePersistedState("cycles", 5);
+  const [endpoint, setEndpoint] = usePersistedState('endpoint', '');
+  const [counts, setCount] = usePersistedState('counts', 100);
+  const [cycles, setCycles] = usePersistedState('cycles', 5);
 
   async function fetcher(graphQLParams: any) {
     const response = await fetch(endpoint, {
-      method: "post",
-      headers: { 
-        "Content-Type": "application/json",
-       },
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify(graphQLParams)
     });
     return await response.json();
   }
 
   async function fetcherBenchmarker(graphQLParams: any) {
-    if (graphQLParams.operationName === "IntrospectionQuery") {
+    if (graphQLParams.operationName === 'IntrospectionQuery') {
       return fetcher(graphQLParams);
     }
 
@@ -89,9 +86,7 @@ export function Editor() {
       console.log(`average duration for cycle ${i + 1}: ${cycleAverage}ms`);
       totalDuration += cycleDuration;
     }
-    console.log(
-      `benchmarking took ${totalDuration}ms for ${counts} requests over ${cycles} cycles`
-    );
+    console.log(`benchmarking took ${totalDuration}ms for ${counts} requests over ${cycles} cycles`);
     return mostRecentResponseBody;
   }
 
@@ -106,23 +101,12 @@ export function Editor() {
         />
 
         <GraphiQL.Menu label="File" title="File">
-          <GraphiQL.MenuItem label="Save" title="Save" onSelect={() => { }} />
+          <GraphiQL.MenuItem label="Save" title="Save" onSelect={() => {}} />
         </GraphiQL.Menu>
 
-        <input
-          value={endpoint}
-          onChange={event => setEndpoint(event.target.value)}
-        />
-        <input
-          value={counts}
-          type={"number"}
-          onChange={event => setCount(parseInt(event.target.value))}
-        />
-        <input
-          value={cycles}
-          type={"number"}
-          onChange={event => setCycles(parseInt(event.target.value))}
-        />
+        <input value={endpoint} onChange={event => setEndpoint(event.target.value)} />
+        <input value={counts} type={'number'} onChange={event => setCount(parseInt(event.target.value))} />
+        <input value={cycles} type={'number'} onChange={event => setCycles(parseInt(event.target.value))} />
       </GraphiQL.Toolbar>
       <GraphiQL.Footer></GraphiQL.Footer>
     </GraphiQL>
--- app/renderer/index.tsx
+++ app/renderer/index.tsx
@@ -1,13 +1,5 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { Editor } from './Editor'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Editor } from './Editor';
 
-
-const render = (Component) => {
-  ReactDOM.render(
-    <Component />,
-    document.getElementById('root')
-  )
-}
-
-render(Editor)
+ReactDOM.render(<Editor />, document.getElementById('root'));
