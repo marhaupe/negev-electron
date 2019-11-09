88296f332b4c288b0c7e1a9dfedf2d8f8f5a24a0
marhaupe <marhaupe97@gmail.com>
2019-11-09 17:13:43 +0100

    change config, fix issues

---
+++ app/index.d.ts
@@ -0,0 +1,3 @@
+declare module 'graphiql';
+declare module 'isomorphic-fetch';
+declare module 'app-root-path';
--- app/main/main.ts
+++ app/main/main.ts
@@ -1,36 +1,39 @@
-const { format } = require('url')
-
-const { BrowserWindow, app } = require('electron')
-const isDev = require('electron-is-dev')
-const { resolve } = require('app-root-path')
+import { format } from 'url';
+import { BrowserWindow, app } from 'electron';
+import { resolve } from 'app-root-path';
+import isDev from 'electron-is-dev';
 
 app.on('ready', async () => {
-  const webPreferences = isDev? {
-      webSecurity: false 
-  } : {}
+  const webPreferences = isDev
+    ? {
+        webSecurity: false
+      }
+    : {};
 
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     show: false,
-    webPreferences,
-  })
+    webPreferences
+  });
 
   mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
-    if (isDev) { mainWindow.webContents.openDevTools() }
-  })
+    mainWindow.show();
+    if (isDev) {
+      mainWindow.webContents.openDevTools();
+    }
+  });
 
-  const devPath = 'http://localhost:1124'
+  const devPath = 'http://localhost:1124';
   const prodPath = format({
     pathname: resolve('app/renderer/.parcel/production/index.html'),
     protocol: 'file:',
     slashes: true
-  })
-  const url = isDev ? devPath : prodPath
+  });
+  const url = isDev ? devPath : prodPath;
 
-  mainWindow.setMenu(null)
-  mainWindow.loadURL(url)
-})
+  mainWindow.setMenu(null);
+  mainWindow.loadURL(url);
+});
 
-app.on('window-all-closed', app.quit)
+app.on('window-all-closed', app.quit);
