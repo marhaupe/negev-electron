309e98d88b167245251df86cf8b74336fabf0786
Marcel Haupenthal <marhaupe97@gmail.com>
2019-11-10 22:54:04 +0100

    add tests

--- packages/graphql-loadtest-core/src/test/executor.test.ts
+++ packages/graphql-loadtest-core/src/test/executor.test.ts
@@ -16,4 +16,41 @@ describe('tests execute', () => {
     const result = await execute(config);
     expect(result).toBeDefined();
   });
+
+  it('tests that the correct amount of requests are being sent in one second', async () => {
+    const arrivalRate = 100;
+    const config = {
+      phases: [{ arrivalRate, duration: 1 }],
+      fetchParams: {
+        body: {
+          operationName: undefined,
+          query: '{name}',
+          variables: null,
+        },
+        url: 'http://example.com/query',
+      },
+    };
+    const result = await execute(config);
+    expect(result).toBeDefined();
+    expect(result.totalRequests).toEqual(arrivalRate);
+  });
+
+  it('tests that the correct amount of requests are being sent in two seconds', async () => {
+    const arrivalRate = 100;
+    const duration = 2;
+    const config = {
+      phases: [{ arrivalRate, duration }],
+      fetchParams: {
+        body: {
+          operationName: undefined,
+          query: '{name}',
+          variables: null,
+        },
+        url: 'http://example.com/query',
+      },
+    };
+    const result = await execute(config);
+    expect(result).toBeDefined();
+    expect(result.totalRequests).toEqual(arrivalRate * duration);
+  });
 });
