8623e57da7f760cc82cbaa1307f77a71f21f1b9c
marhaupe <marhaupe97@gmail.com>
2019-11-25 18:00:19 +0100

    add pause input

--- packages/graphql-loadtest-electron/app/renderer/Settings.tsx
+++ packages/graphql-loadtest-electron/app/renderer/Settings.tsx
@@ -5,6 +5,109 @@ import { useAppConfigContext } from './context';
 export function Settings() {
   const [config, setConfig] = useAppConfigContext();
 
+  return (
+    <div className="container h-full mx-auto py-10">
+      <Link className="block underline mb-3" to={'/'}>
+        Go back
+      </Link>
+      <p className="block text-3xl mb-6 font-bold">Settings</p>
+      {renderPhasesConfig()}
+      {renderHeadersConfig()}
+    </div>
+  );
+
+  function renderPhasesConfig() {
+    return (
+      <div className="border-t-2 border-gray-400 py-8">
+        <button
+          className="underline"
+          onClick={() =>
+            setConfig({ ...config, phases: [...config.phases, { arrivalRate: 1, duration: 1, pause: 0 }] })
+          }
+        >
+          Add phase
+        </button>
+        {config.phases.map((phase, index) => {
+          return (
+            <div key={index} className="my-8 p-6 container mx-auto border rounded-lg shadow-md">
+              <div className="relative">
+                <p className="text-xl font-bold mb-4">Phase {index}</p>
+                <button
+                  onClick={() => {
+                    const newConfig = { ...config };
+                    newConfig.phases.splice(index, 1);
+                    setConfig(newConfig);
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="absolute right-0 top-0 feather feather-trash"
+                  >
+                    <polyline points="3 6 5 6 21 6"></polyline>
+                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                  </svg>
+                </button>
+              </div>
+              <div className="py-2">
+                <label htmlFor="duration">Duration (in seconds)</label>
+                <input
+                  className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                  name="duration"
+                  value={phase.duration}
+                  placeholder={'Duration (in seconds)'}
+                  type={'number'}
+                  onChange={event => {
+                    const newConfig = { ...config };
+                    newConfig.phases[index] = { ...phase, duration: parseInt(event.target.value) };
+                    setConfig(newConfig);
+                  }}
+                />
+              </div>
+              <div className="py-2">
+                <label htmlFor="arrivalRate">Arrival Rate (per second)</label>
+                <input
+                  className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                  name="arrivalRate"
+                  value={phase.arrivalRate}
+                  placeholder={'Arrival Rate (per second)'}
+                  type={'number'}
+                  onChange={event => {
+                    const newConfig = { ...config };
+                    newConfig.phases[index] = { ...phase, arrivalRate: parseInt(event.target.value) };
+                    setConfig(newConfig);
+                  }}
+                />
+              </div>
+              <div className="py-2">
+                <label htmlFor="pause">Pause (in milliseconds)</label>
+                <input
+                  className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                  name="pause"
+                  value={phase.pause}
+                  placeholder={'Pause (in milliseconds)'}
+                  type={'number'}
+                  onChange={event => {
+                    const newConfig = { ...config };
+                    newConfig.phases[index] = { ...phase, pause: parseInt(event.target.value) };
+                    setConfig(newConfig);
+                  }}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
   function renderHeadersConfig() {
     return (
       <div className="border-t-2 border-gray-400 py-8">
@@ -83,90 +186,4 @@ export function Settings() {
       </div>
     );
   }
-
-  function renderPhasesConfig() {
-    return (
-      <div className="border-t-2 border-gray-400 py-8">
-        <button
-          className="underline"
-          onClick={() => setConfig({ ...config, phases: [...config.phases, { arrivalRate: 1, duration: 1 }] })}
-        >
-          Add phase
-        </button>
-        {config.phases.map((phase, index) => {
-          return (
-            <div key={index} className="my-8 p-6 container mx-auto border rounded-lg shadow-md">
-              <div className="relative">
-                <p className="text-xl font-bold mb-4">Phase {index}</p>
-                <button
-                  onClick={() => {
-                    const newConfig = { ...config };
-                    newConfig.phases.splice(index, 1);
-                    setConfig(newConfig);
-                  }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="absolute right-0 top-0 feather feather-trash"
-                  >
-                    <polyline points="3 6 5 6 21 6"></polyline>
-                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                  </svg>
-                </button>
-              </div>
-              <div className="py-2">
-                <label htmlFor="duration">Duration (in seconds)</label>
-                <input
-                  className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-                  name="duration"
-                  value={phase.duration}
-                  placeholder={'Duration (in seconds)'}
-                  type={'number'}
-                  onChange={event => {
-                    const newConfig = { ...config };
-                    newConfig.phases[index] = { ...phase, duration: parseInt(event.target.value) };
-                    setConfig(newConfig);
-                  }}
-                />
-              </div>
-              <div className="py-2">
-                <label htmlFor="arrivalRate">Arrival Rate (per second)</label>
-                <input
-                  className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-                  name="arrivalRate"
-                  value={phase.arrivalRate}
-                  placeholder={'Arrival Rate (per second)'}
-                  type={'number'}
-                  onChange={event => {
-                    const newConfig = { ...config };
-                    newConfig.phases[index] = { ...phase, arrivalRate: parseInt(event.target.value) };
-                    setConfig(newConfig);
-                  }}
-                />
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-
-  return (
-    <div className="container h-full mx-auto py-10">
-      <Link className="block underline mb-3" to={'/'}>
-        Go back
-      </Link>
-      <p className="block text-3xl mb-6 font-bold">Settings</p>
-      {renderPhasesConfig()}
-      {renderHeadersConfig()}
-    </div>
-  );
 }
