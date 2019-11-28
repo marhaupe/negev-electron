0aabeab8ff3fcf61983de067e3fd83603b4f767a
Marcel Haupenthal <marhaupe97@gmail.com>
2019-11-28 21:50:27 +0100

    add proof of concept for streaming of stats

--- packages/graphql-loadtest-core/src/loadtest.ts
+++ packages/graphql-loadtest-core/src/loadtest.ts
@@ -1,4 +1,4 @@
-import { Config, Stats } from './types';
+import { Config, Stats, QueryResult } from './types';
 import { validateConfig } from './validator';
 import { executePhase } from './phase';
 import {
@@ -8,6 +8,37 @@ import {
   calculateMaxDurationPerRequest,
   calculateJitter,
 } from './calculator';
+import Stream from 'stream';
+
+export async function executeStreamingLoadtest(config: Config, stream: Stream.Readable): Promise<void> {
+  const validationResult = validateConfig(config);
+  if (!validationResult.isValid) {
+    throw new Error('config is not valid. ' + validationResult.reason);
+  }
+  const { phases, fetchConfig } = config;
+
+  for (const phase of phases) {
+    const responses = await executePhase(phase, fetchConfig);
+    const totalRequests = responses.length;
+    const combinedDuration = calculateTotalDuration(responses);
+    const averageDurationPerRequest = calculateAverageDurationPerRequest(combinedDuration, totalRequests);
+
+    const minDurationPerRequest = calculateMinDurationPerRequest(responses);
+    const maxDurationPerRequest = calculateMaxDurationPerRequest(responses);
+    const jitter = calculateJitter(maxDurationPerRequest, minDurationPerRequest, averageDurationPerRequest);
+    stream.push(
+      JSON.stringify({
+        totalRequests,
+        responses,
+        averageDurationPerRequest,
+        maxDurationPerRequest,
+        minDurationPerRequest,
+        jitter,
+      })
+    );
+  }
+  stream.push(null);
+}
 
 export async function executeLoadtest(config: Config): Promise<Stats[]> {
   const validationResult = validateConfig(config);
@@ -17,11 +48,11 @@ export async function executeLoadtest(config: Config): Promise<Stats[]> {
 
   const { phases, fetchConfig } = config;
 
-  const responsesPerPhase = await Promise.all(
-    phases.map(async phase => {
-      return await executePhase(phase, fetchConfig);
-    })
-  );
+  const responsesPerPhase: QueryResult[][] = [];
+  for (const phase of phases) {
+    const phaseResult = await executePhase(phase, fetchConfig);
+    responsesPerPhase.push(phaseResult);
+  }
 
   return responsesPerPhase.map(responses => {
     const totalRequests = responses.length;
