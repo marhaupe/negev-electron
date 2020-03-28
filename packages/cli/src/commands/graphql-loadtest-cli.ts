e7fa3ccfbfdc4e4237a130a251a59b84fbb0b40d
marhaupe <marhaupe97@gmail.com>
2020-03-28 17:36:35 +0100

    Improve CLI flow

--- packages/cli/src/commands/graphql-loadtest-cli.ts
+++ packages/cli/src/commands/graphql-loadtest-cli.ts
@@ -2,49 +2,7 @@ import { GluegunCommand, http } from 'gluegun'
 import fetch from 'node-fetch'
 import * as inquirer from 'inquirer'
 import { introspectionQuery, parse, buildClientSchema, validate } from 'graphql'
-
-async function validateQuery(
-  query: string,
-  prevAnswers: Record<string, any>
-): Promise<true | string> {
-  // Removes non-breaking spaces such as \u00A0
-  query = query.replace(/\s+/g, ' ')
-  query = query.replace(/\s/g, ' ')
-
-  const response = await http
-    .create({
-      baseURL: prevAnswers.endpoint,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        ...prevAnswers?.headers,
-      },
-    })
-    .post<any, any>('', {
-      query: introspectionQuery,
-    })
-
-  const schema = buildClientSchema(response?.data?.data)
-  const queryDocument = parse(query)
-  const errors = validate(schema, queryDocument)
-  if (errors && errors.length > 0) {
-    return errors[0].message
-  }
-  return true
-}
-
-async function validateURL(url: string): Promise<true | string> {
-  return fetch(url)
-    .then((response) => {
-      if (response.status === 404) {
-        return 'Invalid URL: 404'
-      }
-      return true
-    })
-    .catch((error) => {
-      return 'Invalid URL: ' + error.toString()
-    })
-}
+import { executeLoadtest } from 'graphql-loadtest'
 
 function validateNumber(input: string): true | string {
   const error = 'Please enter an integer'
@@ -67,7 +25,25 @@ const askEndpoint = {
   name: 'endpoint',
   message: 'What URL should I loadtest for you?',
   default: 'http://localhost:4000',
-  validate: validateURL,
+  validate: async function validateURL(url: string): Promise<true | string> {
+    return fetch(url)
+      .then((response) => {
+        if (response.status === 404) {
+          return 'Invalid URL: 404'
+        }
+        return true
+      })
+      .catch((error) => {
+        return 'Invalid URL: ' + error.toString()
+      })
+  },
+}
+
+const askShoudAskHeaders = {
+  type: 'confirm',
+  name: 'shouldAskHeaders',
+  message: 'Do you want to set any headers?',
+  default: false,
 }
 
 const askHeaders = {
@@ -78,6 +54,7 @@ const askHeaders = {
     '# Content-Type: application/json\n' +
     '# Authorization: Basic d2lraTpwZWRpYQ==\n',
   message: 'Which headers does your query need?',
+  when: (answers: any): boolean => answers.shouldAskHeaders,
   filter: (input: string): any => {
     let lines = input.split('\n')
     lines = lines
@@ -99,44 +76,87 @@ const askQuery = {
   name: 'query',
   default:
     '# Example query:\n' +
-    '#{ \n' +
-    '#  books { \n' +
-    '#    author \n' +
-    '#  } \n' +
-    '#}\n',
+    '{ \n' +
+    '  books { \n' +
+    '    author \n' +
+    '  } \n' +
+    '}\n',
   message: 'What query should I loadtest for you?',
-  validate: validateQuery,
+  validate: async function validateQuery(
+    query: string,
+    answers: Record<string, any>
+  ): Promise<true | string> {
+    // Removes non-breaking spaces such as \u00A0
+    query = query.replace(/\u00A0+/g, ' ')
+
+    const response = await http
+      .create({
+        baseURL: answers.endpoint,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          ...answers?.headers,
+        },
+      })
+      .post<any, any>('', {
+        query: introspectionQuery,
+      })
+
+    const schema = buildClientSchema(response?.data?.data)
+    const queryDocument = parse(query)
+    const errors = validate(schema, queryDocument)
+    if (errors && errors.length > 0) {
+      return errors[0].message
+    }
+    return true
+  },
+}
+
+const askShouldAskNumberRequests = {
+  type: 'confirm',
+  name: 'shouldAskNumberRequests',
+  message: 'Do you want to limit the total number of requests?',
+  default: false,
 }
 
 const askNumberRequests = {
   type: 'number',
   name: 'numberRequests',
-  default: 0,
-  message: 'How many requests do you want to run at most? (0 = no limit)',
+  default: 200,
+  message: 'How many requests do you want to run at most?',
+  when: (answers: any): boolean => answers.shouldAskNumberRequests,
   validate: validateNumber,
 }
 
-const askNumberWorkers = {
-  type: 'number',
-  name: 'numberWorkers',
-  default: 10,
-  message: 'How many workers should concurrently send requests?',
-  validate: validateNumber,
+const askShouldAskRateLimit = {
+  type: 'confirm',
+  name: 'shouldAskRateLimit',
+  message: 'Do you want to limit the number of requests sent per second?',
+  default: false,
 }
 
 const askRateLimit = {
   type: 'number',
   name: 'rateLimit',
-  default: 0,
-  message: 'How many queries should I send per second at most? (0 = no limit)',
+  default: 50,
+  message: 'How many queries should I send per second at most?',
+  when: (answers: any): boolean => answers.shouldAskRateLimit,
   validate: validateNumber,
+}
+
+const askShouldAskDuration = {
+  type: 'confirm',
+  name: 'shouldAskDuration',
+  message: 'Do you want to set a duration?',
+  default: false,
 }
 
 const askDuration = {
   type: 'number',
   name: 'duration',
   default: 10,
-  message: 'How long should the loadtest take? (in seconds)',
+  message: 'How long (in seconds) should the loadtest take?',
+  when: (answers: any): boolean => answers.shouldAskDuration,
   validate: validateNumber,
 }
 
@@ -144,17 +164,29 @@ const command: GluegunCommand = {
   name: 'graphql-loadtest-cli',
   run: async () => {
     const result = await inquirer.prompt([
+      askShoudAskHeaders,
       askHeaders,
       askEndpoint,
       askQuery,
+      askShouldAskDuration,
       askDuration,
+      askShouldAskNumberRequests,
       askNumberRequests,
-      askNumberWorkers,
+      askShouldAskRateLimit,
       askRateLimit,
     ])
 
-    console.log('result', result)
+    const loadtestResult = await executeLoadtest({
+      endpoint: result.endpoint,
+      query: result.query,
+      duration: result.duration,
+      headers: result.headers,
+      numberRequests: result.numberRequests,
+      rateLimit: result.rateLimit,
+    })
+
+    delete loadtestResult.responses
+    console.log(loadtestResult)
   },
 }
-
 export default command
